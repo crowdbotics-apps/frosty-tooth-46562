cd9a96946cdfd8c6271e535c779f8d348227cba8
Crowdbotics <team@crowdbotics.com>
2024-02-22 08:23:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-tooth-46562",
+  url: "https://frosty-tooth-46562.botics.co",
+  api: "https://frosty-tooth-46562.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
